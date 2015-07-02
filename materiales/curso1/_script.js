059e11b8f3a370cd955a8c2a25deb69bc388d40d
Albin Rdz <ealbinu@gmail.com>
2015-07-02 09:45:38 -0500

    Misma función para dos #drops

--- materiales/curso1/_script.js
+++ materiales/curso1/_script.js
@@ -9,19 +9,24 @@ $(function(){
   $('.drag[data="error"]').draggable({revert:"valid"});
   $('.drag[data="ok"]').draggable({revert:"invalid"});
   //
-  $('#drop').droppable({
+  $('#drop, #drop2').droppable({
     drop: function(event, ui){
       //Obtenemos el valor data del objeto arrastrable
       var objeto = ui.draggable.attr('data');
       //checamos el valor data del objeto con una palabra
       if(objeto=='ok'){
         //Si es OK añadimos una alerta
-        $('#drop').after('<div class="alert alert-success">CORRECTO</div>');
-        //Mostrar el ejercicio 2
-        $('#ejercicio2').slideDown(600);
+        $(this).after('<div class="alert alert-success">CORRECTO</div>');
+
+        if($(this).attr('id')=="drop"){
+          //Mostrar el ejercicio 2
+          $('#ejercicio2').slideDown(600);
+        } else if($(this).attr('id')=="drop2"){
+          alert('Terminaste el ejercicio!!');
+        }
       } else {
         //Si no es ok otra alerta
-        $('#drop').after('<div class="alert alert-danger">INCORRECTO</div>');
+        $(this).after('<div class="alert alert-danger">INCORRECTO</div>');
       }
       //Luego de crear las alertas se aplica un fadeout para borrarlas
       $('.alert').fadeOut(1500);
