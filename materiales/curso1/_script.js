9b1508a8940ae7c576c7b5ae2be513545285091a
Albin Rdz <ealbinu@gmail.com>
2015-07-02 09:08:43 -0500

    Clase 6

--- materiales/curso1/_script.js
+++ materiales/curso1/_script.js
@@ -2,4 +2,14 @@ $(function(){
 
   $('#modalInicio').modal('show');
 
+  // ARRASTRABLE
+  $('.drag').draggable();
+  //
+  $('#drop').droppable({
+    drop: function(event, ui){
+      var objeto = ui.draggable.attr('data');
+      alert(objeto);
+    }
+  });
+
 });
