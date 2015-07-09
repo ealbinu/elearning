13f3ea6f574432e5f0d19258af85cf442432fd54
Albin Rdz <ealbinu@gmail.com>
2015-07-09 09:20:55 -0500

    Contador, Correctas y Errores

--- materiales/curso2/_script.js
+++ materiales/curso2/_script.js
@@ -8,8 +8,27 @@ $(function(){
   $('#modalInicio').modal('show');
 
   $('.ejercicio button').click(function(){
+
     contador = contador+1;
 
+    var data = $(this).attr('data');
+
+    if(data == "punto"){
+      correctas = correctas+1;
+    } else if(data=="medio"){
+      correctas = correctas+0.5;
+    } else if(data=="pierde"){
+      errores = errores+1;
+    }
+
+    $('.correctas').html(correctas);
+    $('.errores').html(errores);
+
+    if(contador == respuestas){
+      $('#modalFinal').modal('show');
+    }
+
+    console.log(data);
     console.log(contador);
   });
 
