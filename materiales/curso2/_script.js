b1cdb3081332e89b2996add25c39f699abf6d6c3
Albin Rdz <ealbinu@gmail.com>
2015-07-09 09:05:23 -0500

    Click en botones

--- materiales/curso2/_script.js
+++ materiales/curso2/_script.js
@@ -1,7 +1,18 @@
+var contador = 0;
+var respuestas = 12;
+var correctas = 0;
+var errores = 0;
+
 $(function(){
 
   $('#modalInicio').modal('show');
 
-  
+  $('.ejercicio button').click(function(){
+    contador = contador+1;
+
+    console.log(contador);
+  });
+
+
 
 });
