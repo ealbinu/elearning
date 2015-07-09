57a7808199989608b657e71256c356361fdff241
Albin Rdz <ealbinu@gmail.com>
2015-07-09 10:21:40 -0500

    Cambios

--- materiales/curso3/_script.js
+++ materiales/curso3/_script.js
@@ -1,40 +1,36 @@
-var contador = 0;
-var respuestas = 3;
 var correctas = 0;
-var errores = 0;
-
 $(function(){
 
   $('#modalInicio').modal('show');
 
-  $('.ejercicio button').click(function(){
+  $('.checar').click(function(){
 
-    contador = contador+1;
+    var mouse = $('.inputMouse').val();
+    var mouseData = $('.inputMouse').attr('data');
+    var psd = $('.inputPhotoshop').val();
+    var psdData = $('.inputPhotoshop').attr('data');
+    var google = $('.inputGoogle').val();
+    var googleData = $('.inputGoogle').attr('data');
 
-    var data = $(this).attr('data');
-
-    if(data == "punto"){
+    if(mouse==mouseData){
       correctas = correctas+1;
-    } else if(data=="medio"){
-      correctas = correctas+0.5;
-    } else if(data=="pierde"){
-      errores = errores+1;
+    } else {
+      $('.inputMouse').attr('disabled', 'disabled');
+    }
+    if(psd==psdData){
+      correctas = correctas+1;
+    } else {
+      $('.inputPhotoshop').attr('disabled', 'disabled');
+    }
+    if(google==googleData){
+      correctas = correctas+1;
+    } else {
+      $('.inputGoogle').attr('disabled', 'disabled');
     }
 
+    $('#modalFinal').modal('show');
     $('.correctas').html(correctas);
-    $('.errores').html(errores);
 
-    if(contador == respuestas){
-      $('#modalFinal').modal('show');
-    }
-
-    $(this).slideUp();
-
-
-    console.log(data);
-    console.log(contador);
   });
-
-
 
 });
