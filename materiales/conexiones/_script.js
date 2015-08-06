db86a2e807919fe8e2c4027ec50bf45e43b91411
Albin Rdz <ealbinu@gmail.com>
2015-08-06 09:28:49 -0500

    Checar conexión jsPlumb

--- materiales/conexiones/_script.js
+++ materiales/conexiones/_script.js
@@ -1,21 +1,26 @@
 var correctas = 0;
 
 $(function(){
-
   $('#modalInicio').modal('show');
-
 });
 
 jsPlumb.bind("ready", function(){
 
-    //Opciones de los endpoints
-    var endOpciones = { isSource: true, isTarget:true }
+  //Opciones de los endpoints
+  var endOpciones = {
+    isSource: true,
+    isTarget:true,
+    connectorStyle: { lineWidth:20, strokeStyle: '#fffd00' },
+    beforeDrop: function(params){
+      alert('soltado!');
+      alert(params.sourceId);
+      alert(params.targetId);
+    }
+  }
 
-    var endPoint1 = jsPlumb.addEndpoint(
-      'conector1', {anchor:'AutoDefault'}, endOpciones);
-
-    var endPoint2 = jsPlumb.addEndpoint(
-      'conector2', {anchor:'AutoDefault'}, endOpciones);
-
+  var endPoint1 = jsPlumb.addEndpoint(
+    'conector1', {anchor:'Right'}, endOpciones);
+  var endPoint2 = jsPlumb.addEndpoint(
+    'conector2', {anchor:'Left'}, endOpciones);
 
 });
