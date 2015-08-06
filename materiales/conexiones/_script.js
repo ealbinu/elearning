ed2777658a788d4d03ef91f7272319238c4726b6
Albin Rdz <ealbinu@gmail.com>
2015-08-06 09:12:11 -0500

    Conexión básica JsPlumb

--- materiales/conexiones/_script.js
+++ materiales/conexiones/_script.js
@@ -4,8 +4,18 @@ $(function(){
 
   $('#modalInicio').modal('show');
 
+});
 
+jsPlumb.bind("ready", function(){
 
+    //Opciones de los endpoints
+    var endOpciones = { isSource: true, isTarget:true }
+
+    var endPoint1 = jsPlumb.addEndpoint(
+      'conector1', {anchor:'AutoDefault'}, endOpciones);
+
+    var endPoint2 = jsPlumb.addEndpoint(
+      'conector2', {anchor:'AutoDefault'}, endOpciones);
 
 
 });
